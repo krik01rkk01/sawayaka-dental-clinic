e1d4b55e19e9a08f8511c964f55f6837fa8c1ad2
krik01rkk01 <kri767811rk@gmail.com>
2025-08-20 20:37:15 +0900

    8/21アップロード

--- sawayaka_dental_clinic/assets/js/script.js
+++ sawayaka_dental_clinic/assets/js/script.js
@@ -15,6 +15,30 @@ $(function () {
   });
 });
 
+// about スライダー
+$(document).ready(function(){
+  // メインスライダー
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    fade: true,
+    autoplay: true,        // 自動再生ON
+    autoplaySpeed: 3000,   // 3秒ごと
+    asNavFor: '.slider-nav'
+  });
+
+  // サムネイルスライダー
+  $('.slider-nav').slick({
+    slidesToShow: 5,       // サムネイルの表示数
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    focusOnSelect: true,
+    centerMode: false,
+    arrows: false
+  });
+
+});
 
 
 
